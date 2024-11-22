7fbdb854d257bd7c864c23a14c04c53b735baadc
Polyden <karandeevdenis1@gmail.com>
2024-11-22 13:55:39 +0300

    Пофиксил хуйню

--- t2petr3/Assets/Player.cs
+++ t2petr3/Assets/Player.cs
@@ -1,11 +1,14 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float speed;
-    private bool isMoving;
+    private bool isMoving = false;
     [SerializeField] private float tileSize;
+    private Vector3 _direction;
+    private Vector3 _position;
     void Start()
     {
         
@@ -14,53 +17,64 @@ public class Player : MonoBehaviour
     {
         if(!isMoving)
         {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                _direction = Vector3.up;
 
-            if (Input.GetKey(KeyCode.W))
-            {
-                transform.position += Time.deltaTime * speed * Vector3.up;
+                _position = transform.position;
+                isMoving = true;
             }
-            else if (Input.GetKey(KeyCode.S))
+            else if (Input.GetKeyDown(KeyCode.S))
             {
-                transform.position += Time.deltaTime * speed * Vector3.down;
+                _direction = Vector3.down;
+
+                _position = transform.position;
+                isMoving = true;
             }
-            else if (Input.GetKey(KeyCode.A))
+            else if (Input.GetKeyDown(KeyCode.A))
             {
-                transform.position += Time.deltaTime * speed * Vector3.left;
+                _direction = Vector3.left;
+
+                _position = transform.position;
+                isMoving = true;
             }
-            else if (Input.GetKey(KeyCode.D))
+            else if (Input.GetKeyDown(KeyCode.D))
             {
-                transform.position += Time.deltaTime * speed * Vector3.right;
+                _direction = Vector3.right;
+
+                _position = transform.position;
+                isMoving = true;
             }
         }
-    }
-    private IEnumerator Move(Vector3 direction)
-    {
-        Vector3 curPos = transform.position;
-        isMoving = true;
-        transform.position += Time.deltaTime * speed * direction;
-        yield return new WaitUntil(() => CheckPos(curPos, direction));
-        isMoving = false;
+        else
+        {
+            transform.position += Time.deltaTime * speed * _direction;
+            if(CheckPos(_position, _direction))
+            {
+                isMoving = false;
+            }
+        }
     }
     private bool CheckPos(Vector3 pos, Vector3 direction)
     {
         if (direction == Vector3.up && transform.position.y >= pos.y + tileSize)
         {
-            transform.position = pos += direction;
+            transform.position = pos += direction * tileSize;
             return true;
         }
         else if (direction == Vector3.down && transform.position.y <= pos.y - tileSize)
         {
-            transform.position = pos += direction;
+            transform.position = pos += direction * tileSize;
             return true;
         }
         else if (direction == Vector3.left && transform.position.x <= pos.x - tileSize)
         {
-            transform.position = pos += direction;
+            transform.position = pos += direction * tileSize;
             return true;
         }
         else if (direction == Vector3.right && transform.position.x >= pos.x + tileSize)
         {
-            transform.position = pos += direction;
+            transform.position = pos += direction * tileSize;
             return true;
         }
         return false;
